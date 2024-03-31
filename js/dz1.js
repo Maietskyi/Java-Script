5c013ef1e8181801531e3d45bfcdd573a14a1217
Maietskyi <127688306+Maietskyi@users.noreply.github.com>
2024-03-31 21:27:44 +0300

    Add files via upload

---
+++ js/dz1.js
@@ -0,0 +1,58 @@
+// // 1 домашня робота
+// let name = 'Іван';
+// let city = 'Іван';
+// console.log(`${city}`);
+
+// 2 домашня робота
+// let name = 'Olga';
+// console.log(`привіт ${1}`); //привіт 1
+// console.log(`привіт ${'name'}`); //привіт name
+// console.log(`привіт ${name}`); //привіт Olga
+
+// // 3 домашня робота
+// let a = '5';
+// console.log(Number(a));
+// console.log(typeof Number(a));
+// let b = '13cvb';
+// console.log(Number.parseInt(b));
+// console.log(typeof Number.parseInt(b));
+// let c = '12.9sxdcfgv';
+// console.log(Number.parseFloat(c));
+// console.log(typeof Number.parseFloat(c));
+
+// // 4 домашня робота
+// console.log((0.1 * 10 + 0.2 * 10) / 10); // 0.3
+
+// // 5 домашня робота
+// console.log(Math.max(20, 10, 50, 40));
+
+// // 6 домашня робота
+// console.log(Math.random() * (4 - 2) + 2);
+
+// // 7 домашня робота
+// const message = 'Welcome to Bahamas!';
+// console.log(message.length);
+
+// // 8 домашня робота
+// const message = 'Welcome to Bahamas!';
+// console.log(message.toUpperCase());
+
+// // 9 домашня робота
+// let user = {
+//   name: 'Михайло',
+//   age: 30,
+//   city: 'Львів',
+// };
+
+// delete user.city;
+
+// user['like flowers'] = true;
+
+// console.log(user);
+
+// // 10 домашня робота
+
+// for (let key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
---
+++ js/script.js
@@ -0,0 +1,79 @@
+// console.log(typeof 3);
+// console.log(typeof 'adf');
+// console.log(typeof true);
+// console.log(1 + 2);
+// const value = '3';
+// console.log(typeof value);
+// const value = '3';
+// console.log(Number(value));
+
+// let a = 31;
+// console.log(a);
+
+// const number = Number('51');
+// console.log(Number.isNaN(number));
+
+// const string = Number('qweqwe');
+// console.log(Number.isNaN(string));
+
+// console.log(0.1 + 0.2 === 0.3); //false
+// console.log(0.1 + 0.2); // 0.30000000000000004
+// console.log((0.1 * 10 + 0.2 * 10) / 10); // 0.3
+
+// // 1 домашня робота
+// let name = 'Іван';
+// let city = 'Іван';
+// console.log(`${city}`);
+
+// // 2 домашня робота
+// let name = 'Olga';
+// console.log(`привіт ${1}`); //привіт 1
+// console.log(`привіт ${'name'}`); //привіт name
+// console.log(`привіт ${name}`); //привіт Olga
+
+// // 3 домашня робота
+// let a = '5';
+// console.log(Number(a));
+// console.log(typeof Number(a));
+// let b = '13cvb';
+// console.log(Number.parseInt(b));
+// console.log(typeof Number.parseInt(b));
+// let c = '12.9sxdcfgv';
+// console.log(Number.parseFloat(c));
+// console.log(typeof Number.parseFloat(c));
+
+// // 4 домашня робота
+// console.log((0.1 * 10 + 0.2 * 10) / 10); // 0.3
+
+// // 5 домашня робота
+// console.log(Math.max(20, 10, 50, 40));
+
+// // 6 домашня робота
+// console.log(Math.random() * (4 - 2) + 2);
+
+// // 7 домашня робота
+// const message = 'Welcome to Bahamas!';
+// console.log(message.length);
+
+// // 8 домашня робота
+// const message = 'Welcome to Bahamas!';
+// console.log(message.toUpperCase());
+
+// // 9 домашня робота
+// let user = {
+//   name: 'Михайло',
+//   age: 30,
+//   city: 'Львів',
+//   'like flowers': true,
+// };
+
+// delete user.city;
+
+// console.log(user);
+
+// // 10 домашня робота
+
+// for (let key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
